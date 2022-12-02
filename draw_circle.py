cc1016b70c5a1ebfa6d52b1e7b878f830be30c89
ywjin6533 <ywjin6533@gmail.com>
2022-12-02 16:23:23 +0900

    MidTest

--- draw_circle.py
+++ draw_circle.py
@@ -8,6 +8,10 @@ drawEnd = False
 img = np.zeros((512, 512, 3), np.uint8)
 circleInputData = []
 
+def worstCaseAssess(radius):
+    n = 2 * radius
+    return 2 * n * (n+1) / 3
+
 def circleAssess(circleInput, center, radius):
     sum = 0
     #print(center[0] , center[1])
@@ -15,8 +19,8 @@ def circleAssess(circleInput, center, radius):
         #print("중심: (", center[0] , center[1],"), 그리고 점: (", point[0], point[1], ")")
         distance = radius - math.sqrt(pow(point[0] - center[0], 2) + pow(point[1] - center[1], 2))
         sum += pow(distance, 2)
-    #print(sum)
-    return sum
+    print("오잉?", sum / len(circleInput))
+    return sum / len(circleInput)
     
 def circleInfer(circleInput):
     center = np.asarray(np.rint(np.mean(circleInput , axis = 0)), dtype = int)
@@ -48,13 +52,13 @@ def paint_draw(event, former_x, former_y, flags, param):
                 img = np.zeros((512, 512, 3), np.uint8)
                 drawEnd = False;
                 circleInputData = [];
+            
+            circleInputData.append([current_former_x, current_former_y])
+            
             cv2.line(img, (current_former_x, current_former_y),
                     (former_x, former_y), (0, 0, 255), 5)
             current_former_x = former_x
             current_former_y = former_y
-            
-            #print("(%d, %d)" % (former_x, former_y))
-            circleInputData.append([former_x, former_y])
     
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
@@ -67,7 +71,9 @@ def paint_draw(event, former_x, former_y, flags, param):
         circleInputData.append([former_x, former_y])
         
         center, radius = circleInfer(circleInputData)
-        print(center, radius)
+        worstCase = worstCaseAssess(radius)
+        print(worstCase)
+        print("제 점수는요.." , round(worstCase - circleAssess(circleInputData, center, radius) / worstCase , 1), "점!!")
         cv2.circle(img, center, radius, (255,212,0), 3) 
     return former_x, former_y
 
