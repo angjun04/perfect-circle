226d9b12039b8c87bdcdd4acfb6b7382811659c4
ywjin6533 <ywjin6533@gmail.com>
2022-12-02 15:33:28 +0900

    x누르면 꺼짐 원 찾음

--- draw_circle.py
+++ draw_circle.py
@@ -1,32 +1,74 @@
 # draw line
 
+import math
 import numpy as np
 import cv2 as cv2
 drawing = False  # true if mouse is pressed
-mode = True
-ix, iy = -1, -1
+drawEnd = False
 img = np.zeros((512, 512, 3), np.uint8)
+circleInputData = []
 
+def circleAssess(circleInput, center, radius):
+    sum = 0
+    #print(center[0] , center[1])
+    for point in circleInput:
+        #print("중심: (", center[0] , center[1],"), 그리고 점: (", point[0], point[1], ")")
+        distance = radius - math.sqrt(pow(point[0] - center[0], 2) + pow(point[1] - center[1], 2))
+        sum += pow(distance, 2)
+    #print(sum)
+    return sum
+    
+def circleInfer(circleInput):
+    center = np.asarray(np.rint(np.mean(circleInput , axis = 0)), dtype = int)
+    #return Center
+    
+    UpRadius = 512
+    DownRadius = 0
+    for i in range(20):
+        #print(UpRadius, DownRadius)
+        TargetRadius1 = (2 * UpRadius + DownRadius) / 3
+        TargetRadius2 = (UpRadius + 2 * DownRadius) / 3
+        
+        if circleAssess(circleInput, center, TargetRadius1) >= circleAssess(circleInput, center, TargetRadius2):
+            UpRadius = TargetRadius1
+        else:
+            DownRadius = TargetRadius2
+    
+    return center, round(UpRadius)       
 
 def paint_draw(event, former_x, former_y, flags, param):
-    global current_former_x, current_former_y, drawing, mode
+    global current_former_x, current_former_y, drawing, drawEnd, img, circleInputData
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         current_former_x, current_former_y = former_x, former_y
+        
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
-            if mode == True:
-                cv2.line(img, (current_former_x, current_former_y),
-                         (former_x, former_y), (0, 0, 255), 5)
-                current_former_x = former_x
-                current_former_y = former_y
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
+            if drawEnd == True:
+                img = np.zeros((512, 512, 3), np.uint8)
+                drawEnd = False;
+                circleInputData = [];
             cv2.line(img, (current_former_x, current_former_y),
-                     (former_x, former_y), (0, 0, 255), 5)
+                    (former_x, former_y), (0, 0, 255), 5)
             current_former_x = former_x
             current_former_y = former_y
+            
+            #print("(%d, %d)" % (former_x, former_y))
+            circleInputData.append([former_x, former_y])
+    
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        drawEnd = True;
+        cv2.line(img, (current_former_x, current_former_y),
+                 (former_x, former_y), (0, 0, 255), 5)
+        current_former_x = former_x
+        current_former_y = former_y
+        
+        circleInputData.append([former_x, former_y])
+        
+        center, radius = circleInfer(circleInputData)
+        print(center, radius)
+        cv2.circle(img, center, radius, (255,212,0), 3) 
     return former_x, former_y
 
 
@@ -36,7 +78,7 @@ cv2.setMouseCallback("OpenCV Paint Brush", paint_draw)
 while(1):
     cv2.imshow("OpenCV Paint Brush", img)
     k = cv2.waitKey(1) & 0xFF
-    if k == 27:
+    if k == 27 or cv2.getWindowProperty('OpenCV Paint Brush', cv2.WND_PROP_VISIBLE) < 1:
         break
 
 cv2.destroyAllWindows()
